35a5e27ef936c5ec50d5774dda5fb0729ebb5e78
seth <117851610+Sethispr@users.noreply.github.com>
2025-08-20 16:38:00 -0700

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,46 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import starlightThemeObsidian from 'starlight-theme-obsidian';
+import { createStarlightObsidianPlugin } from 'starlight-obsidian';
+
+const [vault1_StarlightObsidian, vault1_ObsidianSidebarGroup] = createStarlightObsidianPlugin()
+const [vault2_StarlightObsidian, vault2_ObsidianSidebarGroup] = createStarlightObsidianPlugin()
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://sethispr.github.io/cupid-test/',
+	base: '/cupid-test',
+	integrations: [
+		starlight({
+			title: 'Cupid Docs',
+			social: [
+				{
+					icon: 'github', label: 'GitHub', href: 'https://github.com/sethispr/cupid-test',
+				}
+			],
+			customCss: [
+				// Path to custom CSS file
+				'./src/styles/custom.css',
+			],
+			sidebar: [
+				{
+					label: 'Intro',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Intro', slug: 'intro/guide' },
+					],
+				},
+				{
+					label: 'Profile',
+					autogenerate: { directory: 'profile' },
+				},
+			],
+			plugins: [
+				// Set theme
+				starlightThemeObsidian(),
+			]
+		}),
+	],
+});
