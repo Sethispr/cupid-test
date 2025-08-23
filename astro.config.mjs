9e8dce6c87fb809e78bdbb269d486c21d338568d
seth <117851610+Sethispr@users.noreply.github.com>
2025-08-22 21:05:34 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -41,7 +41,7 @@ export default defineConfig({
         },
       ],
       plugins: [
-        starlightThemeBlack(),
+        starlightThemeBlack({}),
         starlightImageZoom(),
       ],
     }),
